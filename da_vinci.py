3ca4a00b5b02a3ea749538a6d7422bc1fce96acc
William Albuquerque <w.j.albuquerque@outlook.com>
2023-02-07 16:47:43 +0000

    Add Point

--- da_vinci.py
+++ da_vinci.py
@@ -1,6 +1,30 @@
+'''
+This script reproduces images with colored, transparent triangles using
+a genetic algorithm
+'''
+
 import argparse
 import pathlib
+import random
+import typing
+
 import PIL.Image as Image
+import PIL.ImageDraw as ImageDraw
+
+class Point(typing.NamedTuple):
+    '''A 2D point'''
+    x: int
+    y: int
+
+    def random(max_x: int, max_y: int) -> typing.Self:
+        '''
+        Returns a random point in the positive quadrant bounded by
+        max_x and max_y
+        '''
+        assert max_x > 0
+        assert max_y > 0
+        return Point(random.randrange(max_x), random.randrange(max_y))
+
 
 # Parse command line arguments
 parser = argparse.ArgumentParser()
