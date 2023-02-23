9b7151a4acd57c4ccbb43e98214328f5b92d3f23
William Albuquerque <w.j.albuquerque@outlook.com>
2023-02-23 20:50:56 +0000

    Generate genotype and phenotype

--- da_vinci.py
+++ da_vinci.py
@@ -8,15 +8,25 @@ import random
 import PIL.Image as Image
 import PIL.ImageDraw as ImageDraw
 
-# Generate a randomly colored triangle
-image = Image.new('RGB', (100, 100))
+# Define image width and height
+WIDTH, HEIGHT = 100, 100
+
+# Generate random genotype
+GENES = 10
+genotype = {}
+for _ in range(GENES):
+    triangle = tuple(
+        (random.randrange(WIDTH), random.randrange(HEIGHT))
+        for _ in range(3)
+    )
+    color = tuple(random.randrange(256) for _ in range(4))
+    genotype[triangle] = color
+
+# Create phenotype
+image = Image.new('RGB', (WIDTH, HEIGHT))
 draw = ImageDraw.Draw(image, 'RGBA')
-triangle = tuple(
-    (random.randrange(image.width), random.randrange(image.height))
-    for _ in range(3)
-)
-color = tuple(random.randrange(256) for _ in range(4))
-draw.polygon(triangle, color)
+for triangle, color in genotype.items():
+    draw.polygon(triangle, color)
 
 # Save image
 try:
